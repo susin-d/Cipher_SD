2b139e0e482fee0b600a16d1e9c60b1f66cd32ef
susin-d <181512045+susin-d@users.noreply.github.com>
2025-07-10 17:25:45 +0530

    Update webui.bat

--- webui.bat
+++ webui.bat
@@ -4,6 +4,7 @@ if not exist venv (
 )
 call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
+python install_torch.py
 python -m pip install -r requirements.txt
 start http://127.0.0.1:5000/
 python main.py
